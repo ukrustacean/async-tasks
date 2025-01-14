1acf109cfcda1ac99217b35db7ac257b9f99cbeb
Ivan Dereviankin <ukrustacean@gmail.com>
2025-01-14 23:26:15 +0100

    Refactor it into a module

--- Program.fs
+++ Program.fs
@@ -2,33 +2,31 @@
 open System.Threading
 open System
 
-let calculate n =
-    if n > 0 then n |> Math.Sqrt |> Ok else Error "Number should not be lesser then zero!"
-
-let callbackMap (mapping: 'a -> Result<'ok, 'err>) callback list =
-    let results = new List<'ok>()
-    let errors = new List<'err>()
-
-    for item in list do
-        let t =
-            Thread(fun () ->
-                item
-                |> mapping
-                |> function
-                    | Ok v -> lock results (fun () -> results.Add v)
-                    | Error e -> lock errors (fun () -> errors.Add e))
-
-        t.Start()
-
-    while results.Count + errors.Count <> List.length list do
-        Thread.Sleep 100
-
-    callback results errors
-
-let main () =
-    let l = [ 1; 2; 3; -4; 4; 5 ]
-
-    l
-    |> callbackMap calculate (fun a b ->
-        printfn $"Calculated: {String.Join(' ', Seq.toArray a)}"
-        printfn $"Got errors: {String.Join(' ', Seq.toArray b)}")
+module Task1 =
+    let calculate n =
+        if n > 0 then n |> Math.Sqrt |> Ok else Error "Number should not be lesser then zero!"
+    
+    let callbackMap (mapping: 'a -> Result<'ok, 'err>) callback list =
+        let results = new List<'ok>()
+        let errors = new List<'err>()
+    
+        for item in list do
+            let t =
+                Thread(fun () ->
+                    item
+                    |> mapping
+                    |> function
+                        | Ok v -> lock results (fun () -> results.Add v)
+                        | Error e -> lock errors (fun () -> errors.Add e))
+    
+            t.Start()
+    
+        while results.Count + errors.Count <> List.length list do
+            Thread.Sleep 100
+    
+        callback results errors
+    
+    let task1 () =
+        [ 1; 2; 3; -4; 4; 5 ] |> callbackMap calculate (fun a b ->
+            printfn $"Calculated: {String.Join(' ', Seq.toArray a)}"
+            printfn $"Got errors: {String.Join(' ', Seq.toArray b)}")
