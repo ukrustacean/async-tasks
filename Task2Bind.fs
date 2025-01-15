ca5692f676c3c4c14256fd5bf25cc3a97ba941be
Ivan Dereviankin <ukrustacean@gmail.com>
2025-01-15 22:38:28 +0100

    Add missing part of the task

--- Task2Bind.fs
+++ Task2Bind.fs
@@ -28,3 +28,24 @@ let waitAllTasks (l: Task<'a> list) =
         result <- i.Bind(fun i' -> result.Bind(fun result' -> pureTask <| i' :: result'))
 
     result
+
+let promiseMap (mapping: 'a -> Task<Result<'ok, 'err>>) callback list =
+    let either left right =
+        function
+        | Ok v -> left v
+        | Error err -> right err
+
+    let left a (l, r) = (a :: l, r)
+    let right a (l, r) = (l, a :: r)
+
+    (waitAllTasks <| List.map mapping list)
+        .Bind(fun list' ->
+            let results, errors = List.fold (fun s t -> either left right t s) ([], []) list'
+            pureTask <| callback results errors)
+
+let task2 () =
+    ([ 1; 2; 3; -4; 4; 5 ]
+     |> promiseMap calculate (fun a b ->
+         printfn $"Calculated: {String.Join(' ', Seq.toArray a)}"
+         printfn $"Got errors: {String.Join(' ', Seq.toArray b)}"))
+        .Wait()
